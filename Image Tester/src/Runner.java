4a1c879538791978fc6209036b456a1d3af079ee
marika.holz <marika.holz@MU-C1-08.mullen.pvt.k12.co.us>
2022-05-24 13:10:41 -0600

    initial

---
+++ Image Tester/src/Runner.java
@@ -0,0 +1,16 @@
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class Runner
+	{
+
+		public static void main(String[] args)
+			{
+			
+				ImageIcon icon = new ImageIcon(("cauldron.jpg"));;
+				JFrame frame = new JFrame();
+				
+			}
+
+	}
